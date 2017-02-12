1371a41026392086828cfc03a9a8ca3ba939db43
Xavi <mrjavoman@hotmail.com>
2017-02-12 12:18:01 -0600

    Adding show post view

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -13,8 +13,10 @@ class PostIndex extends Component {
         return this.props.posts.map((post) => {
             return (
                 <li className="list-group-item" key={post.id} >
-                    <span className="pull-xs-right">{post.categories}</span>
-                    <strong>{post.title}</strong>
+                    <Link to={"/posts/" + post.id}>
+                        <span className="pull-xs-right">{post.categories}</span>
+                        <strong>{post.title}</strong>
+                    </Link>
                 </li>
             );
         });
@@ -30,7 +32,7 @@ class PostIndex extends Component {
                 </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
-                    {this.renderPosts()}
+                    {this.renderPosts()} 
                 </ul>
             </div>
         );
@@ -38,7 +40,6 @@ class PostIndex extends Component {
 }
 
 function mapStateToProps(state) {
-    console.log(state.posts.all);
     return { posts: state.posts.all };
 }
 
--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,13 +1,12 @@
-//import FETCH_POSTS from '../actions/index';
+import { FETCH_POSTS, FETCH_POST }from '../actions/index';
 
 const INITIAL_STATE = { all: [], post: null };
-const FETCH_POSTS = "FETCH_POSTS";
 
 export default function(state = INITIAL_STATE, action) {
 
-    console.log(FETCH_POSTS);
-
     switch(action.type) {
+        case FETCH_POST:
+            return { ...state, post: action.payload.data }
         case FETCH_POSTS: 
             return { ...state, all: action.payload.data }
         default: 
